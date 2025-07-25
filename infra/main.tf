538875bd329cf6136fea80a946fd8f41de70cb02
mohamed fawzi <mohamedfawzy@gmail.com>
2025-07-25 13:47:44 +0300

    update readme fikel

--- infra/main.tf
+++ infra/main.tf
@@ -18,7 +18,7 @@ module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   version         = "20.24.0"
   cluster_name    = var.cluster_name
-  cluster_version = "1.30"
+  cluster_version = "1.33"
   subnet_ids      = module.vpc.private_subnets
   vpc_id          = module.vpc.vpc_id
 
